46493c9cac81172cb76bbca5733e832ed9148de2
hanialabadla9410 <hanialabadla.9410@gmail.com>
2025-01-10 20:35:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let scrollUp = document.querySelector("span.up");
+
+window.onscroll = function () {
+  window.scrollY >= 500
+    ? scrollUp.classList.add("active")
+    : scrollUp.classList.remove("active");
+};
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
